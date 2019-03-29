cdbac24b23da7f7ba7999dbcd384a9f082fb0559
Arne Molland <arneolaim@gmail.com>
2019-03-29 13:42:38 +0100

    :ambulance: FAST BETA FIX

--- lib/ui/scanner/qr_reader.dart
+++ lib/ui/scanner/qr_reader.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:qr_mobile_vision/qr_camera.dart';
 typedef DynamicCallback(dynamic value);
 
 class QrReader extends StatefulWidget {
   final DynamicCallback onScanned;
+  final DynamicCallback onError;
 
-  QrReader({this.onScanned, key}) : super(key: key);
   QrReader({this.onScanned,this.onError, key}) : super(key: key);
 
   _QrReaderState createState() => _QrReaderState();
@@ -27,6 +27,24 @@ class _QrReaderState extends State<QrReader> {
   @override
   Widget build(BuildContext context) {
     // TODO Implement QR scanner
-    return Container();
+    return SizedBox(
+      width: MediaQuery.of(context).size.width*0.8,
+      height: MediaQuery.of(context).size.height*0.5,
+      child: QrCamera(
+        notStartedBuilder: _qrPlaceholder,
+        qrCodeCallback: widget.onScanned,
+        onError: (context, error) => widget.onError(error),
+      ),
+    );
+  }
+
+  Widget _qrPlaceholder(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width*0.8,
+      height: MediaQuery.of(context).size.height*0.5,
+      child: Center(
+        child: Icon(Icons.camera),
+      )
+    );
   }
 }
