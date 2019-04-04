676463e963cb05e518aa285fbda8f4c7d45340e7
Arne Molland <arneolaim@gmail.com>
2019-04-04 16:36:26 +0200

    :globe_with_meridians: Model changes & tweaks

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'dart:core';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:expo/data/models/serializers.dart';
+import 'package:expo/data/models/user_model.dart';
+import 'dart:convert';
 import 'package:expo/constants.dart';
 import 'package:http/http.dart';
 
@@ -9,12 +12,21 @@ class UserRepository {
 
   Future<void> createOrUpdateUser(FirebaseUser user) async {
     final token = await user.getIdToken();
-    await post(kApiUrl + '/users', headers: {'firebasetoken': token});
+    return await post(kApiUrl + '/users', headers: {'firebasetoken': token});
   }
 
   Future<void> updateUser(FirebaseUser user) async {
     final token = await user.getIdToken();
-    await post(kApiUrl + '/users',
+    return await post(kApiUrl + '/users',
         headers: {'name': user.displayName, 'firebasetoken': token});
+  }
+
+  Future<UserEntity> fetchCurrentUser() async {
+    final user = await FirebaseAuth.instance.currentUser();
+    final token = await user.getIdToken();
+
+    return await get(kApiUrl + '/users/', headers: {'firebasetoken': token }).then((response) {
+      return serializers.deserializeWith(UserEntity.serializer, jsonDecode(response.body));
+    });
   }
 }
